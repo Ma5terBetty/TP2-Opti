12ad65027ed99fba2546084d02aac38cd65ad3fb
Ma5terBetty <tomassossich77@gmail.com>
2023-05-24 16:36:35 -0300

    Copy does not use collisions anymore

--- Assets/Scripts/CopyController.cs
+++ Assets/Scripts/CopyController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CopyController : EntityBase
+public class CopyController : MonoBehaviour
 {
     [SerializeField] private PlayerController player;
     private Vector3 _reverseDirection;
